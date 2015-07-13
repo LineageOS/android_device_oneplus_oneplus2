c8d692a3d16057d8dfc12b45ddbbbc52340fe623
dianlujitao <dianlujitao@gmail.com>
2015-07-13 13:54:42 +0800

    oneplus2: Remove audio_policy.msm8994 from PRODUCT_PACKAGES
     * Deprecated.
    
    Change-Id: I8da281dcb4a8851afa509cf7dbfd925bc0c2536a

--- device.mk
+++ device.mk
@@ -83,7 +83,6 @@ PRODUCT_PACKAGES += \
     audio.primary.msm8994 \
     audio.r_submix.default \
     audio.usb.default \
-    audio_policy.msm8994 \
     libaudio-resampler \
     libqcompostprocbundle \
     libqcomvisualizer \
