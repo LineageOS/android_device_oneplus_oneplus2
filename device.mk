eb816be8505a38c3cdd563316e4af4c99eb6bf83
Myles Watson <mylesgw@google.com>
2017-03-31 11:25:04 -0700

    bluetooth: build libbt-vendor
    Bug: 36810308
    Test: build
    Change-Id: Iae4a6340a184dde818603b546470c8cf883f2f7b

--- device.mk
+++ device.mk
@@ -101,7 +101,8 @@ PRODUCT_COPY_FILES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
-    android.hardware.bluetooth@1.0-impl
+    android.hardware.bluetooth@1.0-impl \
+    libbt-vendor
 
 # Camera
 PRODUCT_PACKAGES += \
