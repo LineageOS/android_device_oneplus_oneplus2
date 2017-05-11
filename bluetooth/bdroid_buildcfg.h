5eaae0c25fa1443c779da5979cad3d8908b2ee7e
Umesh Vats <uvats@codeaurora.org>
2017-05-11 12:03:55 -0700

    oneplus2: Bluetooth: Increase number of SEPs to support SHO/Mcast
    Icreased number of SEPs to support SHO/Mcast
    
    Change-Id: I3cfb4e8d4d6969e442406bd095a865d49947151d

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -22,4 +22,7 @@
 #define MAX_L2CAP_CHANNELS    16
 #define BLE_VND_INCLUDED   TRUE
 #define BTM_SCO_ENHANCED_SYNC_ENABLED FALSE
+
+/* Increasing SEPs to 12 from 6 to support SHO/MCast i.e. two streams per codec */
+#define AVDT_NUM_SEPS 12
 #endif
