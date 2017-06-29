9bf5d9873d97c1d867bba11d811ffaf234882864
Suman Mukherjee <sumam@codeaurora.org>
2017-06-29 17:22:35 +0530

    oneplus2: camera: mm-camera-interface: Camera Daemon late start issue fix
    In Android O, Cameraporvider service is introduced newly and its
    starting before main service which results in getnumofcameras failure.
    Added re-try count to fix this issue.
    
    Change-Id: I3a3ee1cbe6ad983d5ad7266f154b33b93d303804

--- camera/QCamera2/stack/mm-camera-interface/src/mm_camera_interface.c
+++ camera/QCamera2/stack/mm-camera-interface/src/mm_camera_interface.c
@@ -1509,6 +1509,7 @@ void sort_camera_info(int num_cam)
 uint8_t get_num_of_cameras()
 {
     int rc = 0;
+    int i = 0;
     int dev_fd = -1;
     struct media_device_info mdev_info;
     int num_media_devices = 0;
@@ -1603,7 +1604,15 @@ uint8_t get_num_of_cameras()
     cfg.cfgtype = CFG_SINIT_PROBE_WAIT_DONE;
     cfg.cfg.setting = NULL;
     if (ioctl(sd_fd, VIDIOC_MSM_SENSOR_INIT_CFG, &cfg) < 0) {
-        CDBG_ERROR("failed");
+        CDBG("failed...Camera Daemon may not up so try again");
+        for(i = 0; i < MM_CAMERA_EVT_ENTRY_MAX; i++) {
+            if (ioctl(sd_fd, VIDIOC_MSM_SENSOR_INIT_CFG, &cfg) < 0) {
+                CDBG("failed...Camera Daemon may not up so try again");
+                continue;
+            }
+            else
+                break;
+        }
     }
     close(sd_fd);
     dev_fd = -1;
