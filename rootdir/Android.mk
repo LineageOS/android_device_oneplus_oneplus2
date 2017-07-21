071c787b863e2d39eb2f7573aeefab480ef0bd4f
OzzysCmAcc <oskarkratochvil86@yahoo.com>
2017-07-21 18:23:49 +0200

    oneplus2: add support for core_ctl
    Change-Id: Ide5f017819ae9b08d383fe9dad58f29430146b89

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -11,6 +11,14 @@ LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE		:= init.oneplus2.power.sh
+LOCAL_MODULE_TAGS	:= optional eng
+LOCAL_MODULE_CLASS	:= EXECUTABLES
+LOCAL_SRC_FILES		:= etc/init.oneplus2.power.sh
+LOCAL_MODULE_PATH	:= $(TARGET_OUT_EXECUTABLES)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE		:= init.qcom.power.rc
 LOCAL_MODULE_TAGS	:= optional eng
 LOCAL_MODULE_CLASS	:= ETC
