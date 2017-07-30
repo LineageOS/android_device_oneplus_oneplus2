e488b7c4583ea27c0c1b0fdccf0a830b774e51e6
OzzysCmAcc <oskarkratochvil86@yahoo.com>
2017-07-30 22:01:48 +0200

    oneplus2: add recovery init file
    Change-Id: I95a3adb746edcd4deedc930e8d4ecd37c57c9149

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -59,6 +59,14 @@ LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE		:= init.recovery.rc
+LOCAL_MODULE_TAGS	:= optional eng
+LOCAL_MODULE_CLASS	:= ETC
+LOCAL_SRC_FILES		:= etc/init.recovery.rc
+LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE		:= ueventd.qcom.rc
 LOCAL_MODULE_TAGS	:= optional eng
 LOCAL_MODULE_CLASS	:= ETC
