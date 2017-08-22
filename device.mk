28c2c33e026247ced15663bb7eff85c9489b4334
Luca Stefani <luca020400@lineageos.org>
2017-08-22 22:15:21 +0200

    oneplus2: Build Gatekeeper HIDL HAL interface
    Change-Id: I4140f7f17188900d23562e9dfc1e351e1988335f

--- device.mk
+++ device.mk
@@ -170,6 +170,10 @@ PRODUCT_PACKAGES += \
     sap.conf \
     xtwifi.conf
 
+# Gatekeeper
+PRODUCT_PACKAGES += \
+    android.hardware.gatekeeper@1.0-impl
+
 # IPv6
 PRODUCT_PACKAGES += \
     ebtables \
