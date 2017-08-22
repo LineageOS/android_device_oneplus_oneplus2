bca149f8242f3301df008e3e68578ee08f5e8417
Luca Stefani <luca020400@lineageos.org>
2017-08-22 22:07:43 +0200

    oneplus2: Build Audio HIDL HAL interfaces
    Change-Id: Ide624d3008739b68a1b4cac347237e72cd4b4b20

--- device.mk
+++ device.mk
@@ -75,7 +75,12 @@ PRODUCT_PACKAGES += \
 
 # Audio
 PRODUCT_PACKAGES += \
-    audiod \
+    android.hardware.audio@2.0-impl \
+    android.hardware.audio.effect@2.0-impl \
+    android.hardware.broadcastradio@1.0-impl \
+    android.hardware.soundtrigger@2.0-impl
+
+PRODUCT_PACKAGES += \
     audio.a2dp.default \
     audio.primary.msm8994 \
     audio.r_submix.default \
