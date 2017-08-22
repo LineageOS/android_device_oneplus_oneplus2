0af2478c20630e352759aac6a9c50155fb1220a6
Luca Stefani <luca020400@lineageos.org>
2017-08-22 22:03:01 +0200

    oneplus2: Build Display HIDL HAL interfaces
     * Enable binderized Gralloc HAL along with it
    
    Change-Id: I61c34a531e3131209a08b5b4670e1f1f8fbb435d

--- device.mk
+++ device.mk
@@ -137,6 +137,13 @@ PRODUCT_PACKAGES += \
 
 # Display
 PRODUCT_PACKAGES += \
+    android.hardware.graphics.allocator@2.0-impl \
+    android.hardware.graphics.allocator@2.0-service \
+    android.hardware.graphics.composer@2.1-impl \
+    android.hardware.graphics.mapper@2.0-impl \
+    android.hardware.memtrack@1.0-impl
+
+PRODUCT_PACKAGES += \
     copybit.msm8994 \
     gralloc.msm8994 \
     hwcomposer.msm8994 \
