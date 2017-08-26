98e4ff0bb41f84600845dd9cc3fc8bc5d7cbed8b
OzzysCmAcc <oskarkratochvil86@yahoo.com>
2017-08-26 09:07:18 +0200

    oneplus2: fix init for oreo
    Change-Id: Id234f3fc82a431ed42d08801ff9765c26176b9c8

--- init/Android.mk
+++ init/Android.mk
@@ -5,6 +5,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 LOCAL_C_INCLUDES := system/core/init
 LOCAL_CFLAGS := -Wall
+LOCAL_STATIC_LIBRARIES := libbase
 LOCAL_SRC_FILES := init_oneplus2.cpp
 LOCAL_MODULE := libinit_oneplus2
 
