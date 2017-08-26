0ee598ce79ca103e630dd9bd0f8461375f8632a5
OzzysCmAcc <oskarkratochvil86@yahoo.com>
2017-08-26 09:04:31 +0200

    oneplus2: fix moved include in camerawrapper
    Change-Id: I738b3711d91bea9ec2441af65a3ee3d92252cf15

--- camera/Android.mk
+++ camera/Android.mk
@@ -45,7 +45,7 @@ LOCAL_SRC_FILES := \
     CameraWrapper.cpp
 
 LOCAL_SHARED_LIBRARIES := \
-    libhardware liblog libgui libutils
+    libhardware liblog libsensor libutils
 
 LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_MODULE := camera.$(TARGET_BOARD_PLATFORM)
