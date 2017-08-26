394a6328fe97467d399eed9223a4c364bc5c9997
OzzysCmAcc <oskarkratochvil86@yahoo.com>
2017-08-26 09:04:31 +0200

    oneplus2: fix moved include in camerawrapper
    Change-Id: I738b3711d91bea9ec2441af65a3ee3d92252cf15

--- camera/CameraWrapper.cpp
+++ camera/CameraWrapper.cpp
@@ -29,7 +29,7 @@
 #include <hardware/hardware.h>
 #include <hardware/camera.h>
 #include <utils/threads.h>
-#include <gui/SensorManager.h>
+#include <sensor/SensorManager.h>
 
 static android::Mutex gCameraWrapperLock;
 static camera_module_t *gVendorModule = 0;
