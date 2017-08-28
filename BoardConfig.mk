410766f8326600e0520323cce467989dd0fb0eee
Luca Stefani <luca020400@lineageos.org>
2017-08-28 16:05:06 +0200

    oneplus2: Move to fs config gen
    Change-Id: Iacba15145adb4176ac8c88df3dd4b48d139f1365

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -124,7 +124,7 @@ BOARD_USES_QCNE := true
 ENABLE_CPUSETS := true
 
 # Filesystem
-TARGET_ANDROID_FILESYSTEM_CONFIG_H := $(PLATFORM_PATH)/android_filesystem_config.h
+TARGET_FS_CONFIG_GEN := $(PLATFORM_PATH)/config.fs
 
 # GPS
 TARGET_NO_RPC := true
