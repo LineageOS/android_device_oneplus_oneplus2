c298848c9c45fdb830815d6c4dd171b0ce263b88
OzzysCmAcc <oskarkratochvil86@yahoo.com>
2017-08-30 18:28:38 +0200

    oneplus2: update cpu configuration
    Change-Id: Ib00b69c4bc14fb26ce4ea513480cdce3dff13f2d

--- rootdir/etc/init.oneplus2.power.sh
+++ rootdir/etc/init.oneplus2.power.sh
@@ -49,20 +49,17 @@ write /sys/devices/system/cpu/cpu4/cpufreq/scaling_max_freq 960000
 # /sys/. These files receive the default label "sysfs".
 restorecon -R /sys/module/msm_performance/parameters
 
-# Limit A57 max freq from msm_perf module in case CPU 4 is offline
-write /sys/module/msm_performance/parameters/cpu_max_freq "4:960000 5:960000 6:960000 7:960000"
-
 # configure governor settings for little cluster
 write /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor interactive
 restorecon -R /sys/devices/system/cpu # must restore after interactive
 write /sys/devices/system/cpu/cpu0/cpufreq/interactive/use_sched_load 1
 write /sys/devices/system/cpu/cpu0/cpufreq/interactive/use_migration_notif 1
-write /sys/devices/system/cpu/cpu0/cpufreq/interactive/above_hispeed_delay "19000 960000:39000 1248000:29000"
-write /sys/devices/system/cpu/cpu0/cpufreq/interactive/go_hispeed_load 95
-write /sys/devices/system/cpu/cpu0/cpufreq/interactive/timer_rate 20000
+write /sys/devices/system/cpu/cpu0/cpufreq/interactive/above_hispeed_delay 19000
+write /sys/devices/system/cpu/cpu0/cpufreq/interactive/go_hispeed_load 99
+write /sys/devices/system/cpu/cpu0/cpufreq/interactive/timer_rate 19000
 write /sys/devices/system/cpu/cpu0/cpufreq/interactive/hispeed_freq 960000
 write /sys/devices/system/cpu/cpu0/cpufreq/interactive/io_is_busy 1
-write /sys/devices/system/cpu/cpu0/cpufreq/interactive/target_loads "80 960000:95 1248000:99"
+write /sys/devices/system/cpu/cpu0/cpufreq/interactive/target_loads "65 460000:75 960000:80"
 write /sys/devices/system/cpu/cpu0/cpufreq/interactive/min_sample_time 40000
 write /sys/devices/system/cpu/cpu0/cpufreq/interactive/max_freq_hysteresis 80000
 write /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq 384000
@@ -72,12 +69,12 @@ write /sys/devices/system/cpu/cpu4/cpufreq/scaling_governor interactive
 restorecon -R /sys/devices/system/cpu # must restore after interactive
 write /sys/devices/system/cpu/cpu4/cpufreq/interactive/use_sched_load 1
 write /sys/devices/system/cpu/cpu4/cpufreq/interactive/use_migration_notif 1
-write /sys/devices/system/cpu/cpu4/cpufreq/interactive/above_hispeed_delay "19000 1400000:39000 1700000:19000"
-write /sys/devices/system/cpu/cpu4/cpufreq/interactive/go_hispeed_load 90
-write /sys/devices/system/cpu/cpu4/cpufreq/interactive/timer_rate 20000
+write /sys/devices/system/cpu/cpu4/cpufreq/interactive/above_hispeed_delay 19000
+write /sys/devices/system/cpu/cpu4/cpufreq/interactive/go_hispeed_load 99
+write /sys/devices/system/cpu/cpu4/cpufreq/interactive/timer_rate 19000
 write /sys/devices/system/cpu/cpu4/cpufreq/interactive/hispeed_freq 1248000
 write /sys/devices/system/cpu/cpu4/cpufreq/interactive/io_is_busy 1
-write /sys/devices/system/cpu/cpu4/cpufreq/interactive/target_loads "90 1248000:95 1800000:70"
+write /sys/devices/system/cpu/cpu4/cpufreq/interactive/target_loads "70 960000:80 1248000:85"
 write /sys/devices/system/cpu/cpu4/cpufreq/interactive/min_sample_time 40000
 write /sys/devices/system/cpu/cpu4/cpufreq/interactive/max_freq_hysteresis 80000
 write /sys/devices/system/cpu/cpu4/cpufreq/scaling_min_freq 384000
@@ -90,9 +87,6 @@ write /sys/devices/system/cpu/cpu5/online 1
 write /sys/devices/system/cpu/cpu6/online 1
 write /sys/devices/system/cpu/cpu7/online 1
 
-# Restore CPU 4 max freq from msm_performance
-write /sys/module/msm_performance/parameters/cpu_max_freq "4:4294967295 5:4294967295 6:4294967295 7:4294967295"
-
 # input boost configuration
 write /sys/module/cpu_boost/parameters/input_boost_freq "0:1344000"
 write /sys/module/cpu_boost/parameters/input_boost_ms 40
@@ -100,10 +94,11 @@ write /sys/module/cpu_boost/parameters/input_boost_ms 40
 # Configure core_ctl module parameters
 write /sys/devices/system/cpu/cpu4/core_ctl/max_cpus 4
 write /sys/devices/system/cpu/cpu4/core_ctl/min_cpus 1
-write /sys/devices/system/cpu/cpu4/core_ctl/busy_up_thres 70
-write /sys/devices/system/cpu/cpu4/core_ctl/busy_down_thres 20
+write /sys/devices/system/cpu/cpu4/core_ctl/busy_up_thres 60
+write /sys/devices/system/cpu/cpu4/core_ctl/busy_down_thres 30
 write /sys/devices/system/cpu/cpu4/core_ctl/offline_delay_ms  100
 write /sys/devices/system/cpu/cpu4/core_ctl/task_thres 4
+write /sys/devices/system/cpu/cpu0/core_ctl/not_preferred 0
 write /sys/devices/system/cpu/cpu4/core_ctl/is_big_cluster 1
 write /sys/devices/system/cpu/cpu0/core_ctl/max_cpus 4
 write /sys/devices/system/cpu/cpu0/core_ctl/min_cpus 0
@@ -111,13 +106,14 @@ write /sys/devices/system/cpu/cpu0/core_ctl/busy_up_thres 20
 write /sys/devices/system/cpu/cpu0/core_ctl/busy_down_thres 5
 write /sys/devices/system/cpu/cpu0/core_ctl/offline_delay_ms 5000
 write /sys/devices/system/cpu/cpu0/core_ctl/not_preferred 1
+write /sys/devices/system/cpu/cpu0/core_ctl/is_big_cluster 0
 chown system:system /sys/devices/system/cpu/cpu4/core_ctl/min_cpus
 chown system:system /sys/devices/system/cpu/cpu4/core_ctl/max_cpus
 
 # Setting B.L scheduler parameters
 write /proc/sys/kernel/sched_migration_fixup 1
 write /proc/sys/kernel/sched_small_task 30
-write /proc/sys/kernel/sched_upmigrate 99
+write /proc/sys/kernel/sched_upmigrate 95
 write /proc/sys/kernel/sched_downmigrate 85
 write /proc/sys/kernel/sched_freq_inc_notify 400000
 write /proc/sys/kernel/sched_freq_dec_notify 400000
