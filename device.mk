857eaf1dc2c4dfea560a4fc8774aacc26d36311f
Luca Stefani <luca020400@lineageos.org>
2017-09-01 19:15:18 -0400

    oneplus2: Build camera HAL
    Change-Id: I886a801759410af17960d1e29942aa7be9f8770f

--- device.mk
+++ device.mk
@@ -113,6 +113,10 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
+    camera.device@1.0-impl \
+    android.hardware.camera.provider@2.4-impl
+
+PRODUCT_PACKAGES += \
     camera.msm8994 \
     libshim_camera \
     libshim_ims-camera \
