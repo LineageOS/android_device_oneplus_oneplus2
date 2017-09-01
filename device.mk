9d76bb22b699ede53fa6afdc821e35e40466ccd1
Luca Stefani <luca020400@lineageos.org>
2017-09-01 19:12:08 -0400

    op2: Build DRM HAL
    Change-Id: I015993bfa2dd609dbb15f9de6280d4c20ba66dbd

--- device.mk
+++ device.mk
@@ -118,6 +118,10 @@ PRODUCT_PACKAGES += \
     libcnefeatureconfig \
     services-ext
 
+# DRM
+PRODUCT_PACKAGES += \
+	android.hardware.drm@1.0-impl
+
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
