9054799196e94570fdf6ff87870a35a71d95d460
Shreesha <shreeshu10@gmail.com>
2017-09-01 19:47:54 -0400

    oneplus2: Switch to HIDL multihal
    Change-Id: Ie664ca106d120499769e92cc3d96d32f3b65bfea

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -183,9 +183,6 @@ TARGET_NO_RPC := true
 include device/qcom/sepolicy/sepolicy.mk
 BOARD_SEPOLICY_DIRS += $(PLATFORM_PATH)/sepolicy
 
-# Sensors
-USE_SENSOR_MULTI_HAL := true
-
 # Time services
 BOARD_USES_QC_TIME_SERVICES := true
 
