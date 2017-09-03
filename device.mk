a1754535e8d6e0d2d8358567a143cf5e44200d66
OzzysCmAcc <oskarkratochvil86@yahoo.com>
2017-09-03 11:09:58 +0200

    oneplus2: add HIDL display hal
    Change-Id: I69ed3b534abc5bb99343b05224b34d9cdd53b5c6

--- device.mk
+++ device.mk
@@ -122,6 +122,14 @@ PRODUCT_PACKAGES += \
     liboverlay \
     libtinyxml
 
+PRODUCT_PACKAGES += \
+    android.hardware.graphics.composer@2.1-impl \
+    android.hardware.graphics.allocator@2.0-impl \
+    android.hardware.graphics.allocator@2.0-service \
+    android.hardware.graphics.mapper@2.0-impl \
+    android.hardware.memtrack@1.0-impl \
+    android.hardware.renderscript@1.0-impl
+
 # Doze mode
 PRODUCT_PACKAGES += \
     OneplusDoze
