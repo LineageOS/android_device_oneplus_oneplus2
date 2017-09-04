32e4abae34bf501fc53605d83bf89f575316fe64
Artem Borisov <dedsa2002@gmail.com>
2017-09-04 17:41:38 +0200

    oneplus2: include deprecated atomic.h
    Change-Id: I13f0eb9bc9232de97b5375262aabe0a25b208071

--- device.mk
+++ device.mk
@@ -120,7 +120,8 @@ PRODUCT_PACKAGES += \
     camera.msm8994 \
     libshim_camera \
     libshim_ims-camera \
-    sensors.hal.tof
+    sensors.hal.tof \
+    libshim_atomic
 
 # Connectivity Engine support (CNE)
 PRODUCT_PACKAGES += \
