c6fba9501764dc84ab13525c736fdd6bbe54fd50
Luca Stefani <luca020400@lineageos.org>
2017-09-04 17:45:01 +0200

    oneplus2: Add rild socket shim
    Change-Id: Id3bb3cea982b57362a714d59176d9fd44fcec349

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -75,3 +75,11 @@ LOCAL_MODULE := libshim_atomic
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 
 include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := rild_socket.c
+LOCAL_MODULE := rild_socket
+LOCAL_MODULE_TAGS := optional
+
+include $(BUILD_SHARED_LIBRARY)
