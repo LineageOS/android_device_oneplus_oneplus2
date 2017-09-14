7ac2be812de17dbe1cb286ed15dd2fea84613898
Ethan Chen <intervigil@gmail.com>
2017-09-14 01:01:28 -0700

    oneplus2: Add seccomp policy
    Change-Id: I1d5017228a4a9318f8a77ef4036532d64718ecbb

--- device.mk
+++ device.mk
@@ -241,6 +241,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     sensors.ssc.wrapper
 
+# Seccomp
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/seccomp/mediacodec.policy:system/vendor/etc/seccomp_policy/mediacodec.policy \
+    $(LOCAL_PATH)/seccomp/mediaextractor.policy:system/vendor/etc/seccomp_policy/mediaextractor.policy
+
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service
