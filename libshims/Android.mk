20895567ae4a1093e7246a8dc44ec153cc954286
OzzysCmAcc <oskarkratochvil86@yahoo.com>
2017-09-18 17:43:51 +0530

    oneplus2: Add IMS shim to fix video calling
    Change-Id: I966b7c53885babe699a64231e34c85ab98fa446d

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -92,3 +92,15 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 
 include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := libshim_mediabuffer.cpp
+
+LOCAL_SHARED_LIBRARIES := libstagefright_foundation libui libgui
+
+LOCAL_MODULE := libshims_ims
+LOCAL_MODULE_TAGS := optional
+LOCAL_MULTILIB := 64
+
+include $(BUILD_SHARED_LIBRARY)
