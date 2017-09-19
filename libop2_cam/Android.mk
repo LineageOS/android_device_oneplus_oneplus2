5ed11f27a351d807379bb883836d2717377b84d4
thecrazyskull <anaskarbila@aospa.co>
2017-09-19 10:01:02 +0530

    oneplus2: deprecate CameraParametersExtra.h
    Change-Id: Idb810c0bab1bb5a1c93583a03021e0b1ddfd0334

---
+++ libop2_cam/Android.mk
@@ -0,0 +1,8 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES := op2_cam.c
+LOCAL_MODULE := libop2_cam
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+include $(BUILD_SHARED_LIBRARY)
