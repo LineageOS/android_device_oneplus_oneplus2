2864f5d93bc55400474ae342ee9c3cef6c7f44cb
Michael Bestas <mkbestas@lineageos.org>
2017-09-24 20:38:07 +0300

    oneplus2: gps: Cleanup makefiles and remove unused loc_api
    Change-Id: Iea3d329dd5f04598d1994eec1f626d9b95b08502

--- gps/utils/Android.mk
+++ gps/utils/Android.mk
@@ -1,6 +1,3 @@
-ifneq ($(BUILD_TINY_ANDROID),true)
-#Compile this library only for builds with the latest modem image
-
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
@@ -25,12 +22,11 @@ LOCAL_SRC_FILES += \
     loc_misc_utils.cpp
 
 LOCAL_CFLAGS += \
-     -fno-short-enums \
-     -D_ANDROID_ \
-     -std=c++11
+    -fno-short-enums \
+    -D_ANDROID_
 
 ifeq ($(TARGET_BUILD_VARIANT),user)
-   LOCAL_CFLAGS += -DTARGET_BUILD_VARIANT_USER
+    LOCAL_CFLAGS += -DTARGET_BUILD_VARIANT_USER
 endif
 
 LOCAL_LDFLAGS += -Wl,--export-dynamic
@@ -41,22 +37,22 @@ LOCAL_C_INCLUDES:= \
 
 LOCAL_COPY_HEADERS_TO:= gps.utils/
 LOCAL_COPY_HEADERS:= \
-   loc_log.h \
-   loc_cfg.h \
-   log_util.h \
-   linked_list.h \
-   msg_q.h \
-   MsgTask.h \
-   LocHeap.h \
-   LocThread.h \
-   LocTimer.h \
-   loc_target.h \
-   loc_timer.h \
-   LocSharedLock.h \
-   platform_lib_abstractions/platform_lib_includes.h \
-   platform_lib_abstractions/platform_lib_time.h \
-   platform_lib_abstractions/platform_lib_macros.h \
-   loc_misc_utils.h
+    loc_log.h \
+    loc_cfg.h \
+    log_util.h \
+    linked_list.h \
+    msg_q.h \
+    MsgTask.h \
+    LocHeap.h \
+    LocThread.h \
+    LocTimer.h \
+    loc_target.h \
+    loc_timer.h \
+    LocSharedLock.h \
+    platform_lib_abstractions/platform_lib_includes.h \
+    platform_lib_abstractions/platform_lib_time.h \
+    platform_lib_abstractions/platform_lib_macros.h \
+    loc_misc_utils.h
 
 LOCAL_MODULE := libgps.utils
 LOCAL_MODULE_OWNER := qcom
@@ -64,7 +60,4 @@ LOCAL_VENDOR_MODULE := true
 
 LOCAL_MODULE_TAGS := optional
 
-LOCAL_PRELINK_MODULE := false
-
 include $(BUILD_SHARED_LIBRARY)
-endif # not BUILD_TINY_ANDROID
