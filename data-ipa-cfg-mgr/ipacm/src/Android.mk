d4b829848dd991eea888496ac38d333341b2e377
Ethan Chen <intervigil@gmail.com>
2017-09-26 08:46:05 -0700

    oneplus2: Move ipacm to vendor
    *also drop default clang flag
    
    Change-Id: I9dfce2d3b2494cc5b38197b5550f7bb79ad741f0

--- data-ipa-cfg-mgr/ipacm/src/Android.mk
+++ data-ipa-cfg-mgr/ipacm/src/Android.mk
@@ -50,11 +50,12 @@ LOCAL_SRC_FILES := IPACM_Main.cpp \
 LOCAL_MODULE := ipacm
 LOCAL_MODULE_TAGS := optional
 
+LOCAL_VENDOR_MODULE := true
+
 LOCAL_SHARED_LIBRARIES := libipanat
 LOCAL_SHARED_LIBRARIES += libxml2
 LOCAL_SHARED_LIBRARIES += libnfnetlink
 LOCAL_SHARED_LIBRARIES += libnetfilter_conntrack
-LOCAL_CLANG := true
 include $(BUILD_EXECUTABLE)
 
 ################################################################################
