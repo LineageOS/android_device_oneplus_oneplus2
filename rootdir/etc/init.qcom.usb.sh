e5851446b09be6016bfaf2d151cfea00ce5a60ce
OzzysCmAcc <oskarkratochvil86@yahoo.com>
2017-10-04 21:03:14 +0200

    oneplus2: ramdisk updates from stock OOS3.6.0
    Change-Id: Ie4808b120a450f39bf61f65bef456be1517065a1

--- rootdir/etc/init.qcom.usb.sh
+++ rootdir/etc/init.qcom.usb.sh
@@ -202,6 +202,8 @@ case "$target" in
     ;;
     "msm8994" | "msm8992")
         echo BAM2BAM_IPA > /sys/class/android_usb/android0/f_rndis_qc/rndis_transports
+        echo 131072 > /sys/module/g_android/parameters/mtp_tx_req_len
+        echo 131072 > /sys/module/g_android/parameters/mtp_rx_req_len
     ;;
 esac
 
