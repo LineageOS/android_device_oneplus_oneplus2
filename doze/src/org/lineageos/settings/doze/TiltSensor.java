c489a8a763c3954cb8c0d6841492d244367fe8a4
Bruno Martins <bgcngm@gmail.com>
2017-10-08 10:20:53 +0100

    oneplus2: Doze: Apply LineageOS rebrand
    Change-Id: I4daa1092494fdf207ef54dc79f787bcbe1d01a08

--- doze/src/org/lineageos/settings/doze/TiltSensor.java
+++ doze/src/org/lineageos/settings/doze/TiltSensor.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2015 The CyanogenMod Project
- * Copyright (C) 2017 The LineageOS Project
+ *               2017 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-package com.cyanogenmod.settings.doze;
+package org.lineageos.settings.doze;
 
 import android.content.Context;
 import android.hardware.Sensor;
