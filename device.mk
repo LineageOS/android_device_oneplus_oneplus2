dbee4fd6a4a8946da4773369e9b1a8d750f99745
Mikhail Naganov <mnaganov@google.com>
2017-12-28 13:29:05 -0800

    oneplus2: Switch to Soundtrigger 2.1 implementation
    Bug: 68823037
    Test: "ok google" from locked screen
    Change-Id: I256bae45bc3c68febe5d7bb4bdbd559d752b7d96

--- device.mk
+++ device.mk
@@ -80,7 +80,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
     android.hardware.audio@2.0-service \
     android.hardware.audio.effect@2.0-impl \
-    android.hardware.soundtrigger@2.0-impl
+    android.hardware.soundtrigger@2.1-impl
 
 PRODUCT_PACKAGES += \
     audio.a2dp.default \
