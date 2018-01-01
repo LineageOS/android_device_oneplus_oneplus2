5bf86000bc31300789746b9bee8ee81e04bb8072
LuK1337 <priv.luk@gmail.com>
2018-01-01 23:10:58 +0100

    oneplus2: Implement on boot TFA98xx calibration
    * TFA amplifier requires to play a calibration
      sound file on each boot otherwise it calibrates
      on the first played sound eg: unlock phone sound.
    
    Change-Id: Ice91c29d14e30ed688007cffff56375b178106c2

--- device.mk
+++ device.mk
@@ -292,6 +292,11 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/seccomp/mediacodec.policy:system/vendor/etc/seccomp_policy/mediacodec.policy \
     $(LOCAL_PATH)/seccomp/mediaextractor.policy:system/vendor/etc/seccomp_policy/mediaextractor.policy
 
+# TFA calibration
+PRODUCT_PACKAGES += \
+    init.tfa.sh \
+    tinyplay
+
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service.op2
