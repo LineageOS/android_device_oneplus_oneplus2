2a66aee38b449f270077d5d25842162baae262f3
OzzysCmAcc <oskarkratochvil86@yahoo.com>
2018-01-05 08:35:25 +0100

    oneplus2: move ipacm to /vendor
    Change-Id: I01c30e0dfbae549808957544c46314dd9d7d0c70

--- data-ipa-cfg-mgr/ipanat/src/Android.mk
+++ data-ipa-cfg-mgr/ipanat/src/Android.mk
@@ -11,8 +11,10 @@ LOCAL_SRC_FILES := ipa_nat_drv.c \
                    ipa_nat_drvi.c
 
 LOCAL_CFLAGS := -DDEBUG
+LOCAL_MODULE_PATH_64 := $(TARGET_OUT_VENDOR)/lib64
 LOCAL_MODULE := libipanat
 LOCAL_MODULE_TAGS := optional
+
 LOCAL_PRELINK_MODULE := false
 LOCAL_CLANG := true
 include $(BUILD_SHARED_LIBRARY)
