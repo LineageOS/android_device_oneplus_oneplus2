288d3908c3d45b6b1b626d31ca1d6f0fd796f939
OzzysCmAcc <oskarkratochvil86@yahoo.com>
2018-01-24 11:41:03 +0100

    Revert "oneplus2: move some proprietary-files to /vendor"
    This reverts commit de23cac8517a4fd95377df05c1c3fba412a63c6c.
    
    Change-Id: Ia9f501d98bdfef905cf50684eb03f5e49468937f

--- config.fs
+++ config.fs
@@ -7,31 +7,31 @@ value:2951
 [AID_RFS_SHARED]
 value:2952
 
-[system/vendor/bin/wcnss_filter]
+[system/bin/wcnss_filter]
 mode: 0755
 user: AID_BLUETOOTH
 group: AID_BLUETOOTH
 caps: BLOCK_SUSPEND
 
-[system/vendor/bin/cnss-daemon]
+[system/bin/cnss-daemon]
 mode: 0755
 user: AID_SYSTEM
 group: AID_SYSTEM
 caps: NET_BIND_SERVICE
 
-[system/vendor/bin/pm-service]
+[system/bin/pm-service]
 mode: 0755
 user: AID_SYSTEM
 group: AID_SYSTEM
 caps: NET_BIND_SERVICE
 
-[system/vendor/bin/imsdatadaemon]
+[system/bin/imsdatadaemon]
 mode: 0755
 user: AID_SYSTEM
 group: AID_SYSTEM
 caps: NET_BIND_SERVICE
 
-[system/vendor/bin/ims_rtp_daemon]
+[system/bin/ims_rtp_daemon]
 mode: 0755
 user: AID_SYSTEM
 group: AID_RADIO
