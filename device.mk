7c3633027d35463cb1036d1cf2b46d7755e2ff23
Edwin Wong <edwinwong@google.com>
2018-01-31 11:59:57 -0800

    oneplus2: Build ClearKey HIDL plugin
    Convert clearkey plugin to HIDL to support drm HAL v1.1.
    Add SELinux policy for android.hardware.drm@1.1-service.clearkey.
    
    Test: CTS test
      ANDROID_BUILD_TOP= ./android-ccts-tradefed run cts-dev
        --module CtsMediaTestCases
        -t android.media.cts.ClearKeySystemTest#testClearKeyPlaybackCenc
    
    bug: 69635855
    Change-Id: I61e9c272c2a2788fd07d5c12921d28c785661b77

--- device.mk
+++ device.mk
@@ -117,7 +117,8 @@ PRODUCT_COPY_FILES += \
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
-    android.hardware.drm@1.0-service
+    android.hardware.drm@1.0-service \
+    android.hardware.drm@1.1-service.clearkey
 
 # Display
 PRODUCT_PACKAGES += \
