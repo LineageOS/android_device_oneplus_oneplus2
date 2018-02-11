7bb9ab59addf99951ea42ac1c6f9e163cad0195d
OzzysCmAcc <oskarkratochvil86@yahoo.com>
2018-02-11 11:21:27 +0100

    oneplus2: update DRM blob and add DRM service
    Change-Id: I1777a9a97a8f55a27b519fe0bf8988efe94d62a6

--- device.mk
+++ device.mk
@@ -125,7 +125,8 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.0-impl
+    android.hardware.drm@1.0-impl \
+    android.hardware.drm@1.0-service
 
 # Display
 PRODUCT_PACKAGES += \
