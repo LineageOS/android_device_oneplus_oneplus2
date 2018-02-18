a048617ae66d17761d4409f8a01ae09c29fa072a
Shreesha <shreeshu10@gmail.com>
2018-02-18 11:20:59 +0100

    oneplus2: init: Set dalvik properties dynamically
    OnePlus2 does come in two variants (3/16 and 4/64). Often 3GB RAM
    folks are ignored. Setting 3072 values for all causes weird native
    crashes on some apps for folks having 3GB variant. So, set it dynamically
    based off their total RAM.
    
    Change-Id: I7f7cf347bbb23925b48c6411e4731db6137d830e

--- device.mk
+++ device.mk
@@ -57,7 +57,6 @@ PRODUCT_COPY_FILES += \
 TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
 
-$(call inherit-product, frameworks/native/build/phone-xxxhdpi-3072-dalvik-heap.mk)
 $(call inherit-product, frameworks/native/build/phone-xxxhdpi-3072-hwui-memory.mk)
 
 # Device uses high-density artwork where available
