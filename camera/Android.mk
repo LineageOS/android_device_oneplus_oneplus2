abe9eb33becfd45c5be654ca5f9cc8900025cf0c
Bruno Martins <bgcngm@gmail.com>
2018-03-28 16:21:30 +0000

    oneplus2: Drop useless flag currently needed for building camera HAL
     * The first makefile under camera folder is already guarded by the
       makefile in the root of the device tree, rendering the flag useless.
    
    Change-Id: I03a87f4769cd76312107e6fd636096d59a31ab08

--- camera/Android.mk
+++ camera/Android.mk
@@ -1,24 +1,17 @@
-ifeq ($(BOARD_BUILD_OP2_CAMERA),true)
+#
+# Copyright (C) 2017-2018 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
-MM_V4L2_DRIVER_LIST += msm8960
-MM_V4L2_DRIVER_LIST += msm8974
-MM_V4L2_DRIVER_LIST += msm8916
-MM_V4L2_DRIVER_LIST += msm8226
-MM_V4L2_DRIVER_LIST += msm8610
-MM_V4L2_DRIVER_LIST += apq8084
-MM_V4L2_DRIVER_LIST += mpq8092
-MM_V4L2_DRIVER_LIST += msm_bronze
-MM_V4L2_DRIVER_LIST += msm8916
-MM_V4L2_DRIVER_LIST += msm8994
-MM_V4L2_DRIVER_LIST += msm8084
-MM_V4L2_DRIVER_LIST += msm8992
-
-ifneq (,$(filter $(MM_V4L2_DRIVER_LIST),$(TARGET_BOARD_PLATFORM)))
-  ifneq ($(strip $(USE_CAMERA_STUB)),true)
-    ifneq ($(BUILD_TINY_ANDROID),true)
-      include $(call all-subdir-makefiles)
-    endif
-  endif
-endif
-
-endif
+include $(call first-makefiles-under,$(call my-dir))
