176a5d062b4e6c4027b898e429297771c3414f3f
Bruno Martins <bgcngm@gmail.com>
2018-04-01 18:03:39 +0100

    oneplus2: Drop unused HWC2 HIDL impl
     * Ever since the Oreo bringup that good ol' hwcomposer 1.5 is being used.
       The fancy HWC2 doesn't (currently?) work properly on Oneplus2 and many
       other 8994 based devices, if not all. There's no point in keeping unused
       interfaces, so simply remove it.
    
    Change-Id: I26335fe2ac7928a4e56fc9198cfb9555c2b8dc4c

--- device.mk
+++ device.mk
@@ -136,7 +136,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
     android.hardware.graphics.allocator@2.0-service \
-    android.hardware.graphics.composer@2.1-impl \
     android.hardware.graphics.mapper@2.0-impl \
     android.hardware.memtrack@1.0-impl
 
