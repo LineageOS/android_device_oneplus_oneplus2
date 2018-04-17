eaee57d486eac21f71024044a3de2109de36c7af
Bruno Martins <bgcngm@gmail.com>
2018-04-17 10:47:03 +0200

    oneplus2: data-ipa-cfg-mgr: Clean up and remove useless scripts
    Change-Id: Ib32f03e2a75159420d0a0778f481b1643cf34200

--- data-ipa-cfg-mgr/Android.mk
+++ data-ipa-cfg-mgr/Android.mk
@@ -1,5 +1,6 @@
 #
 # Copyright (C) 2015 The CyanogenMod Project
+# Copyright (C) 2018 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,6 +14,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-LOCAL_PATH := $(call my-dir)
-
-include $(call first-makefiles-under,$(LOCAL_PATH))
+include $(call first-makefiles-under,$(call my-dir))
