02f1efb79d0ed1e3a21899106be102d45e48007d
Ethan Chen <intervigil@gmail.com>
2018-05-03 22:26:41 -0700

    oneplus2: Build LegacyMM livedisplay HAL
    Change-Id: Ic6609e573d56a69bbd9d610ad1c84a1870428428

--- device.mk
+++ device.mk
@@ -212,6 +212,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     lights.msm8994
 
+# LiveDisplay
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@1.0-service-legacymm
+
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml \
