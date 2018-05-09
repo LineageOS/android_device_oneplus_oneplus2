f9a8735b029d8dfe5233052bdee4f8d4fab1c905
Bruno Martins <bgcngm@gmail.com>
2018-05-09 02:21:19 +0200

    oneplus2: Build custom QTI telephony extension
     * This extension provides the ability to enable/disable SIM cards
       individually (the so-called manual SIM provisioning).
    
     * For real this time, using the open source implementation.
    
    Change-Id: Iaef60140249ca419ddbb9a7bf165538cea9e776c

--- device.mk
+++ device.mk
@@ -272,10 +272,7 @@ PRODUCT_PACKAGES += \
     libcnefeatureconfig \
     librmnetctl \
     libxml2 \
-    rild_socket \
-    telephony-ext
-
-PRODUCT_BOOT_JARS += telephony-ext
+    rild_socket
 
 # Sensors
 PRODUCT_PACKAGES += \
@@ -290,6 +287,13 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     init.tfa.sh \
     tinyplay
+
+# Telephony
+PRODUCT_PACKAGES += \
+    qti-telephony-common
+
+PRODUCT_BOOT_JARS += \
+    telephony-ext
 
 # TextClassifier
 PRODUCT_PACKAGES += \
