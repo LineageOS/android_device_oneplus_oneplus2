55138495e76868f2a7df5ac1eca7298c46452562
Han Wang <wanghan1995315@gmail.com>
2018-06-19 10:32:43 +0200

    oneplus2: Remove leftover shim from device makefile
    * Also alphabetize
    
    Change-Id: I57e5a8db1527c4f221f7749ded6765d69f3f2374

--- device.mk
+++ device.mk
@@ -122,7 +122,6 @@ PRODUCT_PACKAGES += \
     libqomx_core \
     libshim_atomic \
     libshim_camera \
-    libshim_ims-camera \
     sensors.hal.tof \
     Snap
 
@@ -313,6 +312,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl
 
+# Vendor security patch level
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.lineage.build.vendor_security_patch=2017-10-01
+
 # WiFi
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service
@@ -331,10 +334,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf \
     $(LOCAL_PATH)/wifi/WCNSS_cfg.dat:system/etc/wifi/WCNSS_cfg.dat \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:system/etc/wifi/WCNSS_qcom_cfg.ini
-
-# Vendor security patch level
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.lineage.build.vendor_security_patch=2017-10-01
 
 # Inherit from oppo-common
 $(call inherit-product, device/oppo/common/common.mk)
