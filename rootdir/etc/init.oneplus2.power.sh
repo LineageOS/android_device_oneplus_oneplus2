0b693047bef318a936e7072010d0d54c1cb58164
Han Wang <wanghan1995315@gmail.com>
2018-06-24 14:10:06 +0200

    oneplus2: Keep big cluster preferred
    * Was accidentally changed, which made big cluster not used
    
    Change-Id: Ib84e69f11943f36f80aff22af060e7441610b928

--- rootdir/etc/init.oneplus2.power.sh
+++ rootdir/etc/init.oneplus2.power.sh
@@ -100,7 +100,6 @@ write /sys/devices/system/cpu/cpu4/core_ctl/busy_up_thres 60
 write /sys/devices/system/cpu/cpu4/core_ctl/busy_down_thres 30
 write /sys/devices/system/cpu/cpu4/core_ctl/offline_delay_ms  100
 write /sys/devices/system/cpu/cpu4/core_ctl/task_thres 4
-write /sys/devices/system/cpu/cpu4/core_ctl/not_preferred 1
 write /sys/devices/system/cpu/cpu4/core_ctl/is_big_cluster 1
 write /sys/devices/system/cpu/cpu0/core_ctl/max_cpus 4
 write /sys/devices/system/cpu/cpu0/core_ctl/min_cpus 4
@@ -108,7 +107,7 @@ write /sys/devices/system/cpu/cpu0/core_ctl/busy_up_thres 0
 write /sys/devices/system/cpu/cpu0/core_ctl/busy_down_thres 0
 write /sys/devices/system/cpu/cpu0/core_ctl/offline_delay_ms 100
 write /sys/devices/system/cpu/cpu0/core_ctl/task_thres 4
-write /sys/devices/system/cpu/cpu0/core_ctl/not_preferred 0
+write /sys/devices/system/cpu/cpu0/core_ctl/not_preferred 1
 write /sys/devices/system/cpu/cpu0/core_ctl/is_big_cluster 0
 chown system:system /sys/devices/system/cpu/cpu4/core_ctl/min_cpus
 chown system:system /sys/devices/system/cpu/cpu4/core_ctl/max_cpus
