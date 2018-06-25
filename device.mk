3f654b35410e4e7d3fe9f5f622cc0191e9872501
Wang Han <416810799@qq.com>
2018-06-25 17:22:40 +0800

    oneplus2: Remove sensors wrapper
    * It is not used since Oreo and I do not see any benefits
      on N
    
    Change-Id: I8abd897d2676160352ac9cc843e691505698230e

--- device.mk
+++ device.mk
@@ -277,9 +277,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl
 
-PRODUCT_PACKAGES += \
-    sensors.ssc.wrapper
-
 # Seccomp
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/seccomp/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
