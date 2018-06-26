ea2c84cfd100c3a0c6913ba5994c65585bdeecb9
Wang Han <416810799@qq.com>
2018-06-26 14:25:27 +0800

    oneplus2: Use dispatchStrings() for setNetworkSelectionModeManual()
    * Oneplus changes this and causes a type mismatch
    
    Change-Id: Ic35633d91da91bedf5377b50993d00c6edbd2403

--- libril/ril_service.cpp
+++ libril/ril_service.cpp
@@ -1334,8 +1334,8 @@ Return<void> RadioImpl::setNetworkSelectionModeManual(int32_t serial,
 #if VDBG
     RLOGD("setNetworkSelectionModeManual: serial %d", serial);
 #endif
-    dispatchString(serial, mSlotId, RIL_REQUEST_SET_NETWORK_SELECTION_MANUAL,
-            operatorNumeric.c_str());
+    dispatchStrings(serial, mSlotId, RIL_REQUEST_SET_NETWORK_SELECTION_MANUAL,
+            1, operatorNumeric.c_str());
     return Void();
 }
 
