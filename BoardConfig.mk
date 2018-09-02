1164ccd9942486abf4b1b894d3da2401148fc637
Bruno Martins <bgcngm@gmail.com>
2018-09-03 00:40:59 +0200

    oneplus2: Adapt for local LineageHW java overlays
    Change-Id: Icb2b9810217f4b4d442ab83a16f455bd2ed82071

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -108,7 +108,7 @@ USE_DEVICE_SPECIFIC_CAMERA := true
 BOARD_CHARGER_DISABLE_INIT_BLANK := true
 
 # Lineage Hardware
-BOARD_HARDWARE_CLASS += $(PLATFORM_PATH)/lineagehw
+JAVA_SOURCE_OVERLAYS := org.lineageos.hardware|$(PLATFORM_PATH)/lineagehw|**/*.java
 TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/double_tap"
 
 # Filesystem
