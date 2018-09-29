d8b6823fe9b90eab92695f74e24f70253929398b
OzzysCmAcc <oskarkratochvil86@yahoo.com>
2018-09-29 19:21:07 +0200

    oneplus2: adapt sepolicy to pie
    Change-Id: Ie29a9350341bf42e3c6a747de1a2f6666e982552

--- config.fs
+++ config.fs
@@ -7,19 +7,13 @@ value:2951
 [AID_VENDOR_RFS_SHARED]
 value:2952
 
-[system/vendor/bin/wcnss_filter]
+[system/vendor/bin/btnvtool]
 mode: 0755
 user: AID_BLUETOOTH
 group: AID_BLUETOOTH
 caps: BLOCK_SUSPEND
 
 [system/vendor/bin/cnss-daemon]
-mode: 0755
-user: AID_SYSTEM
-group: AID_SYSTEM
-caps: NET_BIND_SERVICE
-
-[system/vendor/bin/pm-service]
 mode: 0755
 user: AID_SYSTEM
 group: AID_SYSTEM
@@ -36,3 +30,15 @@ mode: 0755
 user: AID_SYSTEM
 group: AID_RADIO
 caps: NET_BIND_SERVICE
+
+[system/vendor/bin/pm-service]
+mode: 0755
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: NET_BIND_SERVICE
+
+[system/vendor/bin/wcnss_filter]
+mode: 0755
+user: AID_BLUETOOTH
+group: AID_BLUETOOTH
+caps: BLOCK_SUSPEND
