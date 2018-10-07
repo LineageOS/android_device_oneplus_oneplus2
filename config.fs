7ef1a0e82140e7540c46114b9af6d4c4904e71c9
Bruno Martins <bgcngm@gmail.com>
2018-10-08 00:25:18 +0100

    oneplus2: Create root folders outside of vendor init script
     * The core SELinux policies for vendor_init are being restricted as to
       comply with Treble. Simply adding a local rule to allow creating
       folders under rootfs would be enough, but at the same time defining
       the extra root folders and making use of AID/fs configuration file
       to set perms is more neat and clean. This seems not to be required
       when the partitions are mounted directly under rootfs, however it's
       not the case here because radio and adsp partitions are respectively
       mounted under /firmware/radio and /firmware/adsp and therefore
       /firmware must exist beforehand.
    
    Change-Id: Idabd7ae72e4c4fd9daac5ba3be3e6236f79f251b

--- config.fs
+++ config.fs
@@ -30,3 +30,21 @@ mode: 0755
 user: AID_SYSTEM
 group: AID_SYSTEM
 caps: NET_BIND_SERVICE
+
+[firmware/]
+mode: 0771
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: 0
+
+[bt_firmware/]
+mode: 0771
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: 0
+
+[persist/]
+mode: 0771
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: 0
