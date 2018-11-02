e857a63a77ff96488087b224786f637d629f3828
Wang Han <416810799@qq.com>
2018-11-02 11:20:48 +0800

    oneplus2: Nuke IMS stack
     * Interferes RIL startup and causes reboots because of NoClassDefError
    
    Change-Id: I449dbd48caa9fc64e8193f7e039388383e517674

--- device.mk
+++ device.mk
@@ -190,11 +190,6 @@ PRODUCT_PACKAGES += \
     ebtables \
     ethertypes
 
-# IMS
-PRODUCT_PACKAGES += \
-    ims-ext-common \
-    libshims_ims
-
 # IRQ Balance
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf
