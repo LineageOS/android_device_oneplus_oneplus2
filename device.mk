bee5afa84255555b416c01771c60e21420d9d67a
OzzysCmAcc <oskarkratochvil86@yahoo.com>
2018-11-17 18:02:01 +0100

    oneplus2: switch keymaster to hwbinder
    Change-Id: Ia8451f1828b7a1f44a296ab3fdf772e21f5d0d70

--- device.mk
+++ device.mk
@@ -211,7 +211,8 @@ PRODUCT_COPY_FILES += \
 
 # Keymaster
 PRODUCT_PACKAGES += \
-    android.hardware.keymaster@3.0-impl
+    android.hardware.keymaster@3.0-impl \
+    android.hardware.keymaster@3.0-service
 
 # Lights
 PRODUCT_PACKAGES += \
