28c815b6ddb8fa74b95040a21ae1e996aa45c8ad
Wang Han <416810799@qq.com>
2019-01-09 00:19:56 +0800

    oneplus2: camera: Allow framework to control torch mode
     * From OneplusOSS.
     * Half broken, but need it for torch to work on HAL3.
    
    Change-Id: I24e48065d400f4880ac5e7ad532630188ff5b4d6

--- camera/QCamera2/QCamera2Hal.cpp
+++ camera/QCamera2/QCamera2Hal.cpp
@@ -1,4 +1,4 @@
-/* Copyright (c) 2012-2014, The Linux Foundataion. All rights reserved.
+/* Copyright (c) 2012-2015, The Linux Foundataion. All rights reserved.
 *
 * Redistribution and use in source and binary forms, with or without
 * modification, are permitted provided that the following conditions are
@@ -49,7 +49,7 @@ camera_module_t HAL_MODULE_INFO_SYM = {
     .set_callbacks = qcamera::QCamera2Factory::set_callbacks,
     .get_vendor_tag_ops = qcamera::QCamera3VendorTags::get_vendor_tag_ops,
     .open_legacy = qcamera::QCamera2Factory::open_legacy,
-    .set_torch_mode = NULL,
+    .set_torch_mode = qcamera::QCamera2Factory::set_torch_mode,
     .init  = NULL,
     .reserved = {0}
 };
