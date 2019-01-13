de55479815d7c20921c6895ff4b8e2256f9d8255
Joey <joey@lineageos.org>
2019-01-13 10:21:11 +0100

    oneplus2: Build Trust HAL
    Change-Id: I92706987f8d3094cdfb82e71e5331b7efe69e5df
    Signed-off-by: Joey <joey@lineageos.org>

--- device.mk
+++ device.mk
@@ -278,6 +278,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     textclassifier.bundle1
 
+# Trust HAL
+PRODUCT_PACKAGES += \
+    vendor.lineage.trust@1.0-service
+
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service.basic
