61096562203a865278d27e23044e100fcbd9f826
Michael Bestas <mkbestas@lineageos.org>
2019-01-14 00:49:14 +0200

    oneplus2: Convert lineagehw to vendor.lineage.touch
    Change-Id: Id1faae6e8b44b486312e44d204c584cc35a6b677

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -106,10 +106,6 @@ TARGET_PROCESS_SDK_VERSION_OVERRIDE := \
 # Charger
 BOARD_CHARGER_DISABLE_INIT_BLANK := true
 
-# Lineage Hardware
-JAVA_SOURCE_OVERLAYS := org.lineageos.hardware|$(PLATFORM_PATH)/lineagehw|**/*.java
-TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/double_tap"
-
 # Filesystem
 TARGET_FS_CONFIG_GEN := $(PLATFORM_PATH)/config.fs
 
@@ -164,6 +160,7 @@ TARGET_USES_MKE2FS := true
 # Power
 TARGET_HAS_NO_WLAN_STATS := true
 TARGET_RPM_SYSTEM_STAT := /d/rpm_stats
+TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/double_tap"
 TARGET_USES_INTERACTION_BOOST := true
 
 # QCOM hardware
