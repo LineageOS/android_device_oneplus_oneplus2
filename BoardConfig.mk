24b723fc1050410a159df9aaa9495bf0ab02faf9
Wang Han <416810799@qq.com>
2019-10-22 12:28:37 +0800

    oneplus2: Drop USE_DEVICE_SPECIFIC_CAMERA
     * Deprecated with new project pathmap.
    
    Change-Id: I0d976fdd994b129c0bf2f78c64e39b2690f1010d

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -98,7 +98,6 @@ TARGET_BOARD_INFO_FILE ?= $(PLATFORM_PATH)/board-info.txt
 
 # Camera
 TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS := true
-USE_DEVICE_SPECIFIC_CAMERA := true
 TARGET_PROCESS_SDK_VERSION_OVERRIDE := \
     /system/vendor/bin/mm-qcamera-daemon=22
 
