129198b70ef85858d61e103aca700ac3b4e4385c
Wang Han <416810799@qq.com>
2019-10-25 18:22:55 +0800

    oneplus2: Drop custom loader config for media swcodec
     * C2 codec does not work correctly on legacy devices, resulting
       in stuttering when playing videos and sounds. After dropping
       it, we no longer need this ugly hack too.
    
    This reverts commit 802482f7af3b887efe40f9d56e8ca3cf6bb777b9.
    
    Change-Id: Ib40f8ec08643db066e8c415b1cc37de2390432b3

--- device.mk
+++ device.mk
@@ -66,10 +66,6 @@ PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 # Haters gonna hate..
 PRODUCT_CHARACTERISTICS := nosdcard
 
-# APEX
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/ld.config.txt:$(TARGET_COPY_OUT_SYSTEM)/etc/swcodec/ld.config.txt
-
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
