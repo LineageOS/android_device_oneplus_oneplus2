7255776be530b8371d188f52956349515227e196
Bruno Martins <bgcngm@gmail.com>
2019-11-22 17:20:47 +0000

    oneplus2: Disable RRO overlays
    Doesn't make that much sense on legacy devices anyway.
    
    Change-Id: Icda8fc7411f59f555dfeef7f1ecacb5a92dd94a6

--- device.mk
+++ device.mk
@@ -19,10 +19,6 @@ $(call inherit-product, vendor/oneplus/oneplus2/oneplus2-vendor.mk)
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
-PRODUCT_ENFORCE_RRO_TARGETS := *
-PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
-    $(LOCAL_PATH)/overlay/lineage-sdk
-
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
