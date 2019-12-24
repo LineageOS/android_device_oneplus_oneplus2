1c1d71619577260e99c6c69ab0ced2902d33cb65
Wang Han <416810799@qq.com>
2019-12-24 23:57:47 +0800

    oneplus2: fingerprint: Convert to oneplus2 target
    Change-Id: Idf3948fe42119beb7e5d9fb896615a1c90db9fee

--- fingerprint/service.cpp
+++ fingerprint/service.cpp
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-#define LOG_TAG "android.hardware.biometrics.fingerprint@2.1-service"
+#define LOG_TAG "android.hardware.biometrics.fingerprint@2.1-service.oneplus2"
 
 #include <android/log.h>
 #include <hidl/HidlSupport.h>
