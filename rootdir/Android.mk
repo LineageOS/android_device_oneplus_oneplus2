f69dcfb0fcbf13c38ebe669765a49b3b9a47ae5e
Wang Han <416810799@qq.com>
2020-06-12 14:10:05 +0800

    oneplus2: Move init.recovery.qcom.rc to dedicated path
    Change-Id: I9b775674249eca8a6ad251b212b38f758260439d

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -39,7 +39,7 @@ LOCAL_MODULE		:= init.recovery.qcom.rc
 LOCAL_MODULE_TAGS	:= optional
 LOCAL_MODULE_CLASS	:= ETC
 LOCAL_SRC_FILES		:= etc/init.recovery.qcom.rc
-LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
+LOCAL_MODULE_PATH	:= $(TARGET_RECOVERY_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
