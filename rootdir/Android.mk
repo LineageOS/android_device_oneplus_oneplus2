4ee4bf76f8d07331b1afc9ed8697a16c0a34d389
Wang Han <416810799@qq.com>
2020-06-12 14:08:26 +0800

    oneplus2: Rename init.recovery.oneplus2.rc to init.recovery.qcom.rc
    Change-Id: Iff1a6061890c85d8fc49282d5593a297e785f501

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -35,10 +35,10 @@ LOCAL_MODULE_PATH	:= $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE		:= init.recovery.oneplus2.rc
+LOCAL_MODULE		:= init.recovery.qcom.rc
 LOCAL_MODULE_TAGS	:= optional
 LOCAL_MODULE_CLASS	:= ETC
-LOCAL_SRC_FILES		:= etc/init.recovery.oneplus2.rc
+LOCAL_SRC_FILES		:= etc/init.recovery.qcom.rc
 LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
